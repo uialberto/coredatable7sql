748c6a7cea10150978b5231987f1a2a494d3a39a
Alberto Baigorria <lbaigorria@farmacorp.com>
2023-07-26 23:32:35 -0400

    creacion y configuracion inicial de proyecto

---
+++ UibasoftSolutions/AppWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
