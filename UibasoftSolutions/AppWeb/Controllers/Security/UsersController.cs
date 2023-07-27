314cff2b7ebec4cc59f4941e6c8e35de07107e24
Alberto Baigorria <lbaigorria@farmacorp.com>
2023-07-27 00:33:09 -0400

    implementacion gestionar usuarios

---
+++ UibasoftSolutions/AppWeb/Controllers/Security/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppWeb.Controllers.Security
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
